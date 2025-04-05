94de46da0d258f6cdaf5e79e9b49f29a71caa800
Charles Pitagan <cvpitagan@gmail.com>
2025-04-05 22:17:32 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class DemoController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() throws ParseException {
+        return employeeService.listOfEmployees();
+    }
+
+    @GetMapping("/authors")
+    public String getAllAuthors() {
+        return "Get all authors!";
+    }
+
+    @GetMapping("/books")
+    public String getAllBooks() {
+        return "Get all books!";
+    }
+
+}
